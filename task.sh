c562de45a316b226c1cdfced2d3ad1c905e93eb8
Fahmi Jabbar <58651943+fahmij8@users.noreply.github.com>
2022-03-13 18:10:51 +0700

    Create task.sh

---
+++ task.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+rand=$[$RANDOM % ${#arr[@]}]
+d=`date '+%Y-%m-%dT%H:%M:%SZ'`
+
+h=`date +%H`
+if [ $h -lt 12 ]; then
+  h="Good Morning!"
+elif [ $h -lt 18 ]; then
+  h="Good Afternoon!"
+else
+  h="Good Night!"
+fi
+
+echo <<EOT >> README.md
+### $h 👋 I'm Fahmi Jabbar,
+A Software Engineer from <img src="https://image.flaticon.com/icons/png/128/323/323372.png" width="13"/> <b>Bandung, Indonesia</b>. 
+- 👨🏼‍🎓 Currently a final year undergraduate student at Universitas Pendidikan Indonesia
+- 💡 Passionate to learn about Web Development (especially Frontend), Machine Learning, and Data Science
+- ❤ Love to turn UI design to be fully functional websites!
+
+<h3>👨‍💻 Tech Stack & Tools</h3>
+
+- Experienced in : 
+  <p>
+    <img alt="JavaScript" src="https://img.shields.io/badge/javascript-%23323330.svg?style=flat&logo=javascript&logoColor=%23F7DF1E"/>
+    <img alt="Nodejs" src="https://img.shields.io/badge/-Nodejs-43853d?style=flat&logo=Node.js&logoColor=white" />
+    <img alt="Express.js" src="https://img.shields.io/badge/express.js-%23404d59.svg?style=flat&logo=express&logoColor=%2361DAFB"/>
+    <img alt="TypeScript" src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=flat&logo=typescript&logoColor=white"/>
+    <img alt="Webpack" src="https://img.shields.io/badge/webpack-%238DD6F9.svg?style=flat&logo=webpack&logoColor=black"/>
+    <img alt="Laravel" src="https://img.shields.io/badge/laravel-%23FF2D20.svg?style=flat&logo=laravel&logoColor=white"/>
+    <img alt="Firebase" src="https://img.shields.io/badge/firebase-%23039BE5.svg?style=flat&logo=firebase"/>
+  </p>
+  <p>
+    <img alt="Python" src="https://img.shields.io/badge/python-%2314354C.svg?style=flat&logo=python&logoColor=white"/>
+    <img alt="Flask" src="https://img.shields.io/badge/flask-%23000.svg?style=flat&logo=flask&logoColor=white"/>
+    <img alt="TensorFlow" src="https://img.shields.io/badge/TensorFlow-%23FF6F00.svg?style=flat&logo=TensorFlow&logoColor=white" />
+    <img alt="Keras" src="https://img.shields.io/badge/Keras-%23D00000.svg?style=flat&logo=Keras&logoColor=white"/>
+    <img alt="OpenCV" src="https://img.shields.io/badge/opencv-%23white.svg?style=flat&logo=opencv&logoColor=white"/>
+  </p>
+  <p>
+    <img alt="Arduino" src="https://img.shields.io/badge/-Arduino-00979D?style=flat&logo=Arduino&logoColor=white"/>
+    <img alt="Visual Studio Code" src="https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=flat&logo=visual-studio-code&logoColor=white"/>
+    <img alt="Git" src="https://img.shields.io/badge/git-%23F05033.svg?style=flat&logo=git&logoColor=white"/>
+  </p>
+- Currently learning : 
+  <p>
+    <img alt="MongoDB" src="https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=flat&logo=mongodb&logoColor=white"/>
+    <img alt="Vue" src="https://img.shields.io/badge/vuejs-%2335495e.svg?style=flat&logo=vuedotjs&logoColor=%234FC08D"/>
+    <img alt="React" src="https://img.shields.io/badge/react-%2320232a.svg?style=flat&logo=react&logoColor=%2361DAFB"/>
+    <img alt="Jest" src="https://img.shields.io/badge/-jest-%23C21325?style=flat&logo=jest&logoColor=white"/>
+  </p>
+
+
+<h3>📈 Github Stats</h3>
+<p>
+  <img alt="Profile overview" src="https://raw.githubusercontent.com/fahmij8/github-stats-transparent/output/generated/overview.svg">
+  <img alt="Repo languages overall" src="https://raw.githubusercontent.com/fahmij8/github-stats-transparent/output/generated/languages.svg" />
+  <img alt="Wakatime Weekly stats" src="https://github-readme-stats.vercel.app/api/wakatime?username=fahmij8&layout=compact">
+</p>
+
+<h3>📞📬 Stay in touch</h3>
+<p>
+  <a href="https://www.linkedin.com/in/fahmijabbar/"><img alt="LinkedIn" src="https://img.shields.io/badge/linkedin-%230077B5.svg?style=flat&logo=linkedin&logoColor=white"/></a>
+  <a href="https://stackoverflow.com/users/13434851/fahmi-jabbar"><img alt="Stack Overflow" src="https://img.shields.io/badge/-Stackoverflow-FE7A16?style=flat&logo=stack-overflow&logoColor=white"/></a>
+</p>
+
+Last Updated at $d
+EOT
+
+git config --local user.email "fahmijabbar@upi.edu"
+git config --local user.name "fahmij8"
+git commit -m ":octocat: update at ${d}"
